17faabae8e8f47d5bfb80ac5c8990b4a81799d30
jkm <gimanis2@gmail.com>
2025-07-08 09:47:31 +0900

    runjavascfipt

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("후츠릿 구독 좋아요");
+console.log(2 + 2);
+
+//자바스크립트 주석
+
+//상수
+const youtube = "후츠릿";
+
+//변수
+let comment = "구독 좋아요";
+console.log("🚀 ~ comment:", comment);
+
+console.log("youtube", youtube);
+
+comment = "알림설정";
+
+console.log("🚀 ~ comment:", comment);
